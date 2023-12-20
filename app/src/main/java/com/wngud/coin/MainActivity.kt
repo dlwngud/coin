a6f0aeac38a102a8bb65d9cca34d00b0cc8b013d
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-12-20 20:59:17 +0900

    [feat] bottom navigation과 navigation 연결

--- app/src/main/java/com/wngud/coin/MainActivity.kt
+++ app/src/main/java/com/wngud/coin/MainActivity.kt
@@ -3,11 +3,27 @@ package com.wngud.coin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.wngud.coin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.run {
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
+            val navController = navHostFragment.findNavController()
+            bottomNavigation.setupWithNavController(navController)
+        }
     }
 }
