e920eb35aef4c557b4e6996f23213aa318f788a0
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-12-21 23:52:13 +0900

    [feat] 원화를 보여주는 코인만 필터링

--- app/src/main/java/com/wngud/coin/repository/CoinListRemoteDataSource.kt
+++ app/src/main/java/com/wngud/coin/repository/CoinListRemoteDataSource.kt
@@ -6,6 +6,6 @@ import com.wngud.coin.network.ApiClient
 class CoinListRemoteDataSource(private val api: ApiClient) {
 
     suspend fun getCoinNameList():List<CoinInfo> {
-        return api.getCoinNameList()
+        return api.getCoinNameList().filter { it.market.startsWith("KRW") }
     }
 }
